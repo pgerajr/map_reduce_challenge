0eeb99e929a79f1e45ad695cd416008e99a6761e
Paolo Gera <61483376+pgerajr@users.noreply.github.com>
2020-11-27 18:09:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function novoAluno(nome, idade) {
+    return { nome, idade };
+}
+
+let alunos = [
+    novoAluno("Mario", 23),
+    novoAluno("José", 45),
+    novoAluno("Marcia", 29),
+    novoAluno("João", 35),
+]
+
+function idadeDaTurma(total, aluno) {
+    return total + aluno.idade;
+}
+
+function showResults(result) {
+    let res1 = document.getElementById("result1");
+    let res2 = document.getElementById("result2");
+
+    if (result == "res1") {
+        res1.style.display = "block";
+    } else {
+        res2.style.display = "block";
+    }
+}
+
+function mapArray() {
+    let map = document.getElementById("mapped");
+    let mapped = []
+
+    for (aluno of alunos) {
+        mapped.push(" " + aluno.nome + " tem " + aluno.idade + " anos");
+    }
+
+    showResults("res1");
+
+    map.innerHTML = mapped;
+}
+
+function reduceArray() {
+    let reduce = document.getElementById("reduced");
+    let total = 0;
+
+    for (aluno of alunos) {
+        total = total + aluno.idade;
+    }
+
+    showResults("res2");
+
+    reduce.innerHTML = "A soma das idades é igual a <strong>" + total + "</strong>";
+
+}
